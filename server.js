cc4f747a4ec73dc038a55116a7edaadd135c6c0e
Mohab <gtakiller999@gmail.com>
2025-08-26 17:21:51 +0300

    Log visitor IPs

--- server.js
+++ server.js
@@ -4,23 +4,16 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  // Get visitor IP (accounting for proxies)
-  const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+  // Get visitor IP
+  const ip =
+    req.headers["x-forwarded-for"]?.split(",")[0] || req.socket.remoteAddress;
 
-  // Make a log entry with timestamp
-  const logEntry = `[${new Date().toISOString()}] Visitor IP: ${ip}\n`;
-  fs.appendFileSync("visitors.log", logEntry);
+  console.log(`Visitor IP: ${ip}`);
 
-  // Show the visitor their IP
-  res.send(`
-    <html>
-      <head><title>IP Checker</title></head>
-      <body style="font-family: sans-serif; text-align: center; margin-top: 50px;">
-        <h1>Your IP is: ${ip}</h1>
-        <p>We logged your visit at ${new Date().toLocaleString()}</p>
-      </body>
-    </html>
-  `);
+  const fs = require("fs");
+  fs.appendFileSync("visitors.log", `Visitor IP: ${ip}\n`);
+
+  res.send("Hello visitor!");
 });
 
 app.listen(PORT, () => {
